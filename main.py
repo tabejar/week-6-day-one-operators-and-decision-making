e8ce9b984c039c7eec3cea04840a090f54f987a0
Thomas Bejar <113930376+tabejar@users.noreply.github.com>
2022-09-28 17:10:54 +0000

    removed from lists

--- main.py
+++ main.py
@@ -21,11 +21,15 @@ print(seconds)
 
 # Remove item 1.45 from seconds.
 seconds = [1.23, 1.45, 1.02, 1.11]
-
+seconds.remove(1.45)
+print(seconds)
 
 # Remove items 1.45, 1.02, and 1.11 from seconds.
 seconds = [1.23, 1.45, 1.02, 1.11]
-
+seconds.remove(1.45)
+seconds.remove(1.02)
+seconds.remove(1.11)
+print(seconds)
 
 ################################comparison operators#########################
 #remember....
