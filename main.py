dfbd62cc3cde6eb2f723f1ec2e0ef1229c36ea73
Thomas Bejar <113930376+tabejar@users.noreply.github.com>
2022-09-28 17:37:00 +0000

    more boolean practice

--- main.py
+++ main.py
@@ -106,34 +106,31 @@ print(my_bool)
 
 # Logical Operators Practice #2
 # Create three variables (num1, num2, and num3):
-
 # Inside num1, store the value 36
-
 # Inside num2, stores the result of the operation 72/2
-
 # Inside num3, store the value 48
-
 # Check if num1 is greater than num2, or less than num3. Store the result of that comparison in a variable called my_bool.
-
+num1 = 36
+num2 = 72/2
+num3 = 48
+my_bool = num1 > num2 or num1 < num3
+print(my_bool)
 
 
 # Logical Operators Practice #3
 # Check if the words:
-
 # word1 = "success", and
-
 # word2 = "technology"
-
 # are not found in the sentence below, and store the result (a boolean) in a variable called my_bool:
-
 # "When something is important enough, you do it even if the odds are against you" - Elon Musk
-
-
-
+word1 = "success"
+word2 = "technology"
+quote = "When something is important enough, you do it even if the odds are against you"
+my_bool = word1 in quote and word2 in quote
+print(my_bool)
 
 
 ########################################decision making in python#####################################
-
 
 
 # Decision Making Practice #1
